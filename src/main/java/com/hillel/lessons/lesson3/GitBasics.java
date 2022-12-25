603acb77a5516c3a2114e0ec8487825743af2f09
Oleksandra Zapara <aleksandra.zapar@gmail.com>
2022-12-25 14:55:08 +0200

    added text to GitBasics.java

--- src/main/java/com/hillel/lessons/lesson3/GitBasics.java
+++ src/main/java/com/hillel/lessons/lesson3/GitBasics.java
@@ -2,6 +2,7 @@ package com.hillel.lessons.lesson3;
 
 public class GitBasics {
     public static void main(String[] args) {
-        System.out.println("Merry Christmas!");
+        System.out.print("Merry Christmas!");
+        System.out.println(" And Happy New Year!");
     }
 }
