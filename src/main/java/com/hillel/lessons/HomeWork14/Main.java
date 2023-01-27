dd127fc6ec7aacfd156c6dea2e134c834dc0b754
Oleksandra Zapara <aleksandra.zapar@gmail.com>
2023-01-27 17:23:40 +0200

    added small updates

--- src/main/java/com/hillel/lessons/HomeWork14/Main.java
+++ src/main/java/com/hillel/lessons/HomeWork14/Main.java
@@ -3,20 +3,26 @@ package com.hillel.lessons.HomeWork14;
 public class Main {
     public static void main(String[] args) {
 
-        Person personFirst = new Person("Vasia", 01,10,1983,"vasia@gmail.com", "777522","Proshko", 75.5, 135.2,12558);
+        Person personFirst = new Person("Vasia", 1,10,1983,"vasia@gmail.com",
+                "777522","Proshko", 75.5, 135.2,12558);
         personFirst.printAccountInfo();
-        Person personSecond = new Person("Sasha", 30,01,1990,"sasha@gmail.com", "222222","Petrova", 65.2, 120.5,15000);
+        Person personSecond = new Person("Sasha", 30,3,1990,"sasha@gmail.com",
+                "222222","Petrova", 65.2, 120.5,15000);
         personSecond.printAccountInfo();
-        Person personThird = new Person("Katia", 24,05,1995,"katia@gmail.com", "3435353","Gira", 50.8, 119.0,150);
+        Person personThird = new Person("Katia", 24,12,1995,"katia@gmail.com",
+                "3435353","Gira", 50.8, 119.8,150);
         personThird.printAccountInfo();
-        Person personFourth = new Person("Petia", 04,04,1987,"petia@gmail.com", "1414145","Qwerty", 100.8, 145.5,9999);
+        Person personFourth = new Person("Petia", 14,6,1987,"petia@gmail.com",
+                "1414145","Qwerty", 100.8, 145.5,9999);
         personFourth.printAccountInfo();
 
         personSecond.setWeight(60.8);
         personSecond.setNumberSteps(4820);
         personSecond.printAccountInfo();
 
-        personFourth.setPressure(145);
+        personFourth.setPressure(130);
+        personFourth.setWeight(95.5);
+        personFourth.setSurName("Qwerty1");
         personFourth.printAccountInfo();
     }
 }
--- src/main/java/com/hillel/lessons/HomeWork14/Person.java
+++ src/main/java/com/hillel/lessons/HomeWork14/Person.java
@@ -9,65 +9,13 @@ public class Person {
     private final int dayOfBirth;
     private final int monthOfBirth;
     private final int yearOfBirth;
-
     private final String email;
     private final String phone;
     private String surName;
-
-    public String getSurName() {
-        return surName;
-    }
-
-    public void setSurName(String surName) {
-        this.surName = surName;
-    }
-
     private double weight;
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        if (weight > 0) {
-            this.weight = weight;
-        }
-    }
-
     private double pressure;
-
-    public double getPressure() {
-        return pressure;
-    }
-
-    public void setPressure(double pressure) {
-        if (pressure > 0) {
-            this.pressure = pressure;
-        }
-    }
-
     private int numberSteps;
-
-    public int getNumberSteps() {
-        return numberSteps;
-    }
-
-    public void setNumberSteps(int numberSteps) {
-        if (pressure > 0) {
-            this.numberSteps = numberSteps;
-        }
-    }
-
     private int age;
-    Date date = new Date();
-    LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-    int year  = localDate.getYear();
-
-    public int getAge() {
-        age  = year - yearOfBirth;
-        return age;
-    }
-
     public Person(String name, int dayOfBirth, int monthOfBirth, int yearOfBirth, String email, String phone,
                   String surName, double vaga, double tusk, int numberSteps) {
         this.name = name;
@@ -82,10 +30,59 @@ public class Person {
         this.numberSteps = numberSteps;
     }
 
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        if (weight > 1) {
+            this.weight = weight;
+        }
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        if (pressure > 50 && pressure < 300) {
+            this.pressure = pressure;
+        }
+    }
+
+    public int getNumberSteps() {
+        return numberSteps;
+    }
+
+    public void setNumberSteps(int numberSteps) {
+        if (numberSteps > 0) {
+            this.numberSteps = numberSteps;
+        }
+    }
+
+    public int getAge() {
+        LocalDate localDate =  new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        int year  = localDate.getYear();
+        age  = year - yearOfBirth;
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ' '+ getSurName() + "\n" + this.dayOfBirth + '.' + this.monthOfBirth + '.' + this.yearOfBirth
+                + "\nEmail: " + this.email + ", phone: " + this.phone  + "\nWeight: " + getWeight() + ", pressure: " + getPressure()
+                + ", numberSteps: " + getNumberSteps() + "\n" + getAge() + " years old \n";
+    }
+
     public void printAccountInfo() {
-        System.out.println(this.name + ' '+ getSurName() + "\n" + this.dayOfBirth + '.' + this.monthOfBirth + '.' + this.yearOfBirth + "\n"
-                + "Email: " + this.email + ", phone: " + this.phone +  "\n" + "Weight: " + getWeight() + ", pressure: " + getPressure()
-                + ", numberSteps: " + getNumberSteps() + "\n" + getAge() + " years old");
-        System.out.println();
+        System.out.println(this);
     }
 }
